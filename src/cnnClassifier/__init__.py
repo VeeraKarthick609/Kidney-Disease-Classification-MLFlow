d5da1096fd20aeb718e1a3bb6fa72e722f5690ea
Veera <veerakarthick609@gmail.com>
2024-03-21 15:38:13 +0530

    added DVC

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 import os
 import sys
 import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_dir = "logs"
-log_filepath = os.path.join(log_dir,"running_logs.log")
-os.makedirs(log_dir, exist_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 
 logging.basicConfig(
@@ -14,7 +17,7 @@ logging.basicConfig(
     format= logging_str,
 
     handlers=[
-        logging.FileHandler(log_filepath),
+        logging.FileHandler(LOG_FILE_PATH),
         logging.StreamHandler(sys.stdout)
     ]
 )
