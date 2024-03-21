48e8c9a9949fad4e77b9b424783063406e182cfe
Veera <veerakarthick609@gmail.com>
2024-03-21 05:39:00 +0530

    dvc added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+import os
 
 
 
@@ -46,6 +47,11 @@ except Exception as e:
         raise e
 
 STAGE_NAME = "Evaluation stage"
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/VeeraKarthick609/Kidney-Disease-Classification-MLFlow.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="VeeraKarthick609"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="8b6d6fd6a221a77677996c678696b7a043d082e3"
+
 try:
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -77,7 +77,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
-            mlflow_uri="https://dagshub.com/entbappy/chest-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/VeeraKarthick609/Kidney-Disease-Classification-MLFlow.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
